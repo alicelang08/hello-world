078a5781eea033993bff80a618c9c640f342ba20
Alice Lang <olypia.alice.lang@gmail.com>
2020-11-03 20:46:32 -0700

    first commit

---
+++ src/app/courses.component.ts
@@ -0,0 +1,26 @@
+
+import { Component } from '@angular/core';
+import { CoursesService } from './course/courses.service';
+
+@Component({
+    selector: 'courses',
+    template: `
+    <h2>{{ title }}</h2>
+    <ul>
+        <li *ngFor="let course of courses">
+        {{ course }}
+        </li>
+    </ul>
+    `
+})
+export class CourseComponent {
+    title = "List of courses";
+    courses;
+
+    constructor(service: CoursesService) {
+
+        this.courses = service.getCourses();
+    }
+
+    //Logic for calling an HTTP service 
+}
